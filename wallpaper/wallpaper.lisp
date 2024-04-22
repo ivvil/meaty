35257bf924f0daa48b018d77f3f5390c7a3a7504
Iván Villagrasa <ivvil412@gmail.com>
2024-04-22 15:53:00 +0200

    Fix wallpaper setting
    Now runs swww correctly and added a function to change the wallpaper
    with a transition

--- wallpaper/wallpaper.lisp
+++ wallpaper/wallpaper.lisp
@@ -1,27 +1,30 @@
 (in-package :meaty)
 
-(defparameter *swww-options* "--transition-type grow")
+(defparameter *swww-options* (list "--transition-type" "grow"))
 
 (defun get-cursor-position ()
   (multiple-value-bind (output output-error exit-code)
       (run-program "hyprctl cursorpos" :output :string :ignore-error-status t)
     (if (= exit-code 0)
-		(map 'vector #'parse-integer (split-sequence:split-sequence #\, output))
+        (map 'vector #'parse-integer (split-sequence:split-sequence #\, output))
         (progn
           (format t "Error while running \"hyprctl cursorpos\": ~a~%" output-error)
           nil))))
 
-(defun change-wallpaper (img opt)
-  (bt:make-thread
-   (lambda ()
-	 (let ((exit-code (wait-process (run-program (append '("swww")
-														 img
-														 opt)
-												 :error-output :string
-												 :ignore-error-status t))))
-	   (if (zerop exit-code)
-		   (print :success)
-		   (print :failure))))
-   :name "Waiting for swww"))
+(defun change-wallpaper (img &optional (opt))
+  (multiple-value-bind (output output-error exit-code)
+	  (run-program (append (list "swww" "img" (native-namestring img)) opt)
+				   :error-output :string
+				   :ignore-error-status t)
+	(if (= exit-code 0)
+		output
+        (progn
+          (format t "Error while running \"swww\": ~a~%" output-error)
+          nil))))
+
+(defun change-wallpaper-with-animation (img)
+  (change-wallpaper img (append *swww-options*
+							(list "--transition-pos"
+								  (format nil "~{~a~^,~}" (coerce (get-cursor-position) 'list))))))
 
 
